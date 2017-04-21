342b92e0540891140e41249fb7876e0aefc7e216
Konstantinv Vahrushev <konstantinv@spotoption.com>
2017-04-21 14:57:26 +0300

    added Controller for InfoPages, namespace also, included faq from action

--- infopages/InfoPagesController.php
+++ infopages/InfoPagesController.php
@@ -3,22 +3,13 @@
  * Created by PhpStorm.
  * User: kostyantynvakhrushev
  * Date: 21.04.17
- * Time: 13:16
+ * Time: 14:54
  */
 
 namespace InfoPages;
 
-use Silex\Application;
-use Symfony\Component\HttpFoundation\Request;
 
 class InfoPagesController
 {
-    public function faqAction(Request $request, Application $app ){
 
-
-
-        exit('hello, I am faq');
-
-
-    }
 }
